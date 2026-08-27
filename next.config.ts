394ecff5c8617a8fbb568a2a943b9d132b8a76d6
kiseon77 <kiseon.han77@gmail.com>
2026-08-27 10:37:58 +0900

    feat: sentry, glitchtip 세팅

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+import { withSentryConfig } from "@sentry/nextjs";
 
 const nextConfig: NextConfig = {
   output: "standalone",
 };
 
-export default nextConfig;
+export default withSentryConfig(nextConfig, {
+  silent: true,
+  sourcemaps: {
+    disable: true,
+  },
+  telemetry: false,
+});
