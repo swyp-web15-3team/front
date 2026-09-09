bd24bfe8df3c7542c143dd7d07b6b0a63f449c45
김성호 <tjdgh0313@gmail.com>
2026-09-09 20:15:50 +0900

    ✨ update: 검색페이지 레이아웃 추가

--- app/(main)/search/page.tsx
+++ app/(main)/search/page.tsx
@@ -1,20 +1,45 @@
+'use client';
+
 import { VerticalCard } from '@/components/ui/VerticalCard';
 
 export default function SearchPage() {
+  const products = {
+    imageUrl: 'https://placehold.co/200x150.png',
+    name: '야마자키 12년',
+    originalName: '山崎 | Yamazaki 12yo',
+    discountRate: -42,
+    krPrice: 298000,
+    jpPrice: 168500,
+    jpPriceYen: 18500,
+  };
+
+  const items = Array.from({ length: 50 }, (_, i) => ({
+    ...products,
+    name: `${i + 1} ${products.name}`,
+  }));
   return (
     <>
-      <VerticalCard
-        className="w-60"
-        product={{
-          imageUrl: 'https://placehold.co/200x150.png',
-          name: '야마자키 12년',
-          originalName: '山崎 | Yamazaki 12yo',
-          discountRate: -42,
-          krPrice: 298000,
-          jpPrice: 168500,
-          jpPriceYen: 18500,
-        }}
-      />
+      <div className="mx-auto max-w-300">
+        <input
+          type="text"
+          placeholder="검색하세요"
+          className="mb-4 w-full bg-gray-200 p-2"
+        />
+        {items.length === 0 ? (
+          <div className="flex min-h-100 items-center justify-center">
+            <p>상품이 없습니다</p>
+          </div>
+        ) : (
+          <>
+            <p>{items.length}개</p>
+            <div className="grid grid-cols-2 gap-4 bg-blue-100 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+              {items.map((item, i) => (
+                <VerticalCard key={i} product={item} />
+              ))}
+            </div>
+          </>
+        )}
+      </div>
     </>
   );
 }
