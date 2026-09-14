ceb40c90a517a2edf3b78e9a1fd1b2b8549fe865
김성호 <tjdgh0313@gmail.com>
2026-09-14 19:28:35 +0900

    ✨ update: 헤더, 푸터 디자인 수정 및 검색모달 추가

--- constants/modal.ts
+++ constants/modal.ts
@@ -2,6 +2,7 @@ export const MODAL_ID = {
   SAMPLE1: 'sample1',
   SAMPLE2: 'sample2',
   CREATE_COLLECTION: 'create-collection',
+  SEARCH: 'search',
 } as const;
 
 export type ModalId = (typeof MODAL_ID)[keyof typeof MODAL_ID];
